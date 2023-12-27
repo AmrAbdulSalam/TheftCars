a4ab52b5a40d95dd36a914df71970d00ed18d1b1
unknown <amrsalam@hotmail.com>
2023-12-27 12:42:37 +0200

    Inserting new cars model with country of origin

--- MySQL-LoadData.sql
+++ MySQL-LoadData.sql
@@ -7,7 +7,19 @@ LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS(car_brand,country_of_origin); 
 
 
-SELECT * FROM car
+SELECT * FROM car;
+
+INSERT INTO car
+(car_brand,country_of_origin)
+values('GMC','America');
+
+INSERT INTO car
+(car_brand,country_of_origin)
+values('Acura','Japan');
+
+INSERT INTO car
+(car_brand,country_of_origin)
+values('Pontiac','America');
 
 
 
